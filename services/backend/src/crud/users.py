9321d0d2c5e0a39a667bedfa073051ff5704e3ee
chacychavez <chacychavez@gmail.com>
2024-01-16 19:55:58 +0800

    feat: update packages version

--- services/backend/src/crud/users.py
+++ services/backend/src/crud/users.py
@@ -23,7 +23,7 @@ async def create_user(user) -> UserOutSchema:
     return await UserOutSchema.from_tortoise_orm(user_obj)
 
 
-async def delete_user(user_id, current_user) -> Status:  # UPDATED
+async def delete_user(user_id, current_user) -> Status:
     try:
         db_user = await UserOutSchema.from_queryset_single(Users.get(id=user_id))
     except DoesNotExist:
@@ -33,6 +33,6 @@ async def delete_user(user_id, current_user) -> Status:  # UPDATED
         deleted_count = await Users.filter(id=user_id).delete()
         if not deleted_count:
             raise HTTPException(status_code=404, detail=f"User {user_id} not found")
-        return Status(message=f"Deleted user {user_id}")  # UPDATED
+        return Status(message=f"Deleted user {user_id}")
 
     raise HTTPException(status_code=403, detail=f"Not authorized to delete")
